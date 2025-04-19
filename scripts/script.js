334e87f24bc258145027fa653d11d32d9c6d1a7c
Joni Mäkinen <joni-makinen@live.fi>
2025-04-19 18:32:00 +0300

    Modify the app so that it lists movies only once, with show start times listed next to the images.

--- scripts/script.js
+++ scripts/script.js
@@ -1,86 +1,122 @@
+// A container to wrap all movie containers into
 let showContainers = document.createElement("div");
 
 function listShows(data) {
+  // Contains all shows (i.e. every screening for every movie on present day)
   let shows = data.querySelectorAll("Show");
 
+  // Will contain key-value pairs; keys will be EventID's (i.e. ID's for every distinct movie),
+  // values will be dttmShowStarts (i.e. movie screening start times)
+  let moviesAndTheirStartTimes = {};
+
+  /** Gather all different movies (i.e. movies that run in chosen theatre on present day)
+   *  and their screening/show start times in an object
+   *  The point here is to form a list of all DIFFERENT MOVIES running on present day
+   * {
+   *   movie01: [startTime01],
+   *   movie02: [startTime01, startTime02],
+   *   movie03: [startTime01, startTime02, startTime03]
+   * }
+   */
   for (let show of shows) {
-    // Movie image
-    let image = document.createElement("img");
-    image.src = show
-      .querySelector("Images")
-      .querySelector("EventMediumImagePortrait").textContent;
-
-    // Movie title
-    let title = document.createElement("p");
-    title.innerText = show.querySelector("Title").textContent;
-    title.innerHTML += "<br />";
-
-    // Original title
-    let ogTitle = document.createElement("p");
-    ogTitle.innerText = show.querySelector("OriginalTitle").textContent;
-    ogTitle.innerHTML += "<br />";
-
-    // Production year
-    let year = document.createElement("p");
-    year.innerText = show.querySelector("ProductionYear").textContent;
-
-    // Movie length
-    let length = document.createElement("p");
-    length.innerText = show.querySelector("LengthInMinutes").textContent;
-
-    // Movie genres
-    let genres = document.createElement("p");
-    genres.innerText = show.querySelector("Genres").textContent;
-
-    // Theatre and auditorium
-    let theatreAndAuditorium = document.createElement("p");
-    theatreAndAuditorium.innerText = show.querySelector(
-      "TheatreAndAuditorium"
-    ).textContent;
-
-    // Movie start time
-    let movieStart = document.createElement("p");
-    movieStart.innerText = show.querySelector("dttmShowStart").textContent;
-
-    // Movie end time
-    let movieEnd = document.createElement("p");
-    movieEnd.innerText = show.querySelector("dttmShowEnd").textContent;
-
-    // Ticket sales end time
-    let salesEnd = document.createElement("p");
-    salesEnd.innerText = show.querySelector("ShowSalesEndTime").textContent;
-
-    // Reservation end time
-    let reservationEnd = document.createElement("p");
-    reservationEnd.innerText = show.querySelector(
-      "ShowReservationEndTime"
-    ).textContent;
-
-    // Presentation method and language
-    let presentationMethodAndLanguage = document.createElement("p");
-    presentationMethodAndLanguage.innerText = show.querySelector(
-      "PresentationMethodAndLanguage"
-    ).textContent;
-
-    let showContainer = document.createElement("div");
-    showContainer.className = "showContainer";
-
-    showContainer.append(image);
-    showContainer.append(title);
-    showContainer.append(ogTitle);
-    showContainer.append(year);
-    showContainer.append(length);
-    showContainer.append(genres);
-    showContainer.append(theatreAndAuditorium);
-    showContainer.append(movieStart);
-    showContainer.append(movieEnd);
-    showContainer.append(salesEnd);
-    showContainer.append(reservationEnd);
-    showContainer.append(presentationMethodAndLanguage);
-
-    showContainers.append(showContainer);
+    let eventID = show.querySelector("EventID").textContent;
+    // If moviesAndTheirStartTimes doesn't already include a specific EventID as a key
+    if (!(eventID in moviesAndTheirStartTimes)) {
+      // Set EventID as a key, and dttmShowStart as a value (wrap in an array)
+      moviesAndTheirStartTimes[eventID] = [
+        show.querySelector("dttmShowStart").textContent,
+      ];
+      // If moviesAndTheirStartTimes already includes a specific EventID as a key
+    } else {
+      // Push dttmShowStart into an array
+      moviesAndTheirStartTimes[eventID].push(
+        show.querySelector("dttmShowStart").textContent
+      );
+    }
   }
-  // console.log(showContainers);
+
+  // Gather all start times for a movie in startTimes
+  for (let movie in moviesAndTheirStartTimes) {
+    let startTimes = document.createElement("p");
+    startTimes.innerText = moviesAndTheirStartTimes[movie];
+  }
+
+  // Keeps track of already added movies
+  let alreadyAddedMovies = [];
+
+  for (let show of shows) {
+    ///////////////
+    // if (show.querySelector("EventID").textContent in moviesAndTheirStartTimes) {
+    //   console.log(show.querySelector("EventID").textContent);
+    // }
+    ////////////
+    // Execute if you come across an unseen EventID (i.e. a new movie)
+    if (
+      !alreadyAddedMovies.includes(show.querySelector("EventID").textContent)
+    ) {
+      // Movie image
+      let image = document.createElement("img");
+      image.src = show
+        .querySelector("Images")
+        .querySelector("EventLargeImagePortrait").textContent;
+      image.height = 500;
+
+      // Movie title
+      let title = document.createElement("p");
+      title.innerText = show.querySelector("Title").textContent;
+
+      // Original title
+      let ogTitle = document.createElement("p");
+      ogTitle.innerText = show.querySelector("OriginalTitle").textContent;
+
+      // Production year
+      let year = document.createElement("p");
+      year.innerText = show.querySelector("ProductionYear").textContent;
+
+      // Movie length
+      let length = document.createElement("p");
+      length.innerText = show.querySelector("LengthInMinutes").textContent;
+
+      // Movie genres
+      let genres = document.createElement("p");
+      genres.innerText = show.querySelector("Genres").textContent;
+
+      // Theatre and auditorium
+      let theatreAndAuditorium = document.createElement("p");
+      theatreAndAuditorium.innerText = show.querySelector(
+        "TheatreAndAuditorium"
+      ).textContent;
+
+      // Presentation method and language
+      let presentationMethodAndLanguage = document.createElement("p");
+      presentationMethodAndLanguage.innerText = show.querySelector(
+        "PresentationMethodAndLanguage"
+      ).textContent;
+
+      // A container to wrap one movie and its info into
+      let showContainer = document.createElement("div");
+      showContainer.className = "showContainer";
+
+      showContainer.append(image);
+      showContainer.append(title);
+      // Append original title only if it differs from title
+      if (title.innerText.localeCompare(ogTitle.innerText) != 0)
+        showContainer.append(ogTitle);
+      showContainer.append(year);
+      showContainer.append(length);
+      showContainer.append(genres);
+      showContainer.append(theatreAndAuditorium);
+      showContainer.append(presentationMethodAndLanguage);
+
+      showContainers.append(showContainer);
+
+      // Push EventID into alreadyAddedMovies
+      // (i.e. keep track of already added movies)
+      alreadyAddedMovies.push(show.querySelector("EventID").textContent);
+    }
+  }
+
+  // Append all show containers to the webpage
   content.append(showContainers);
 }
 
@@ -95,6 +131,7 @@ function handleFetch(theatre) {
 }
 
 function handleClick(e) {
+  // Remove all shows from the webpage (in case user changes the theatre)
   showContainers.innerHTML = "<div></div>";
 
   if (e.target.innerText == "Espoo: OMENA") handleFetch(1039);
@@ -148,6 +185,7 @@ function main() {
     "Raisio: LUXE MYLLY",
   ];
 
+  // Create the theatre list
   let list = document.createElement("ul");
   for (let i = 0; i < theatres.length; i++) {
     let item = document.createElement("li");
